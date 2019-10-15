7cbc1d8717a5989c853d9ed33f48d7a966845dbd
afendi <afendiajip94@gmail.com>
2019-10-15 15:30:25 +0700

    colom and row

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(new MaterialApp(
     title: "New Apss",
-    home: new appbar(),
+    home: new funtioncolomn(),
   ));
 }
 
@@ -12,20 +12,18 @@ class iconstyle extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      body : new Center(
-        child: new Container(
-          color: Colors.blue[900],
-          width: 200.0,
-          height: 200.0,
-          child: new Center(
-            child: new Icon(Icons.android),
-          ),
+        body: new Center(
+      child: new Container(
+        color: Colors.blue[900],
+        width: 200.0,
+        height: 200.0,
+        child: new Center(
+          child: new Icon(Icons.android),
         ),
-      )
-    );
+      ),
+    ));
   }
 }
-
 
 class appbar extends StatelessWidget {
   @override
@@ -33,16 +31,86 @@ class appbar extends StatelessWidget {
     // TODO: implement build
     return new Scaffold(
       backgroundColor: Colors.white,
+      appBar: new AppBar(
+        backgroundColor: Colors.red,
+        leading: new Icon(Icons.home),
+        title: new Text("New Apps"),
+        actions: <Widget>[new Icon(Icons.search)],
+      ),
+    );
+  }
+}
 
-       appBar: new AppBar(
-         backgroundColor: Colors.red,
-         leading: new Icon(Icons.home),
-         title: new Text("New Apps"),
-         actions: <Widget>[
-           new Icon(Icons.search)
-         ],
+class funtionrow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      appBar: new AppBar(
+        backgroundColor: Colors.red,
+        leading: new Icon(Icons.home),
+        title: new Text("New Apps"),
+        actions: <Widget>[new Icon(Icons.search)],
+      ),
+      body: new Container(
+        child : new Row(
+          children: <Widget>[
+            new Icon(
+              Icons.computer,
+              size: 100,
+              color: Colors.blue,
+            ),
+            new Icon(
+              Icons.phone,
+              size: 100,
+              color: Colors.blue,
+            ),
+            new Icon(
+              Icons.mouse,
+              size: 100,
+              color: Colors.blue,
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
 
-       ),
+class funtioncolomn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      appBar: new AppBar(
+        backgroundColor: Colors.red,
+        leading: new Icon(Icons.home),
+        title: new Text("New Apps"),
+        actions: <Widget>[new Icon(Icons.search)],
+      ),
+      body: new Container(
+          child : new Column(
+            children: <Widget>[
+              new Icon(
+                Icons.computer,
+                size: 100,
+                color: Colors.blue,
+              ),
+              new Icon(
+                Icons.phone,
+                size: 100,
+                color: Colors.blue,
+              ),
+              new Icon(
+                Icons.mouse,
+                size: 100,
+                color: Colors.blue,
+              ),
+            ],
+          )
+      ),
     );
   }
 }
